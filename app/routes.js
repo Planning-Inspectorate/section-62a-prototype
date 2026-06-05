2b028ff51f5c7b72296f0e12a05eb72d9c2bf0c9
kierandelacruz-solirius <kieran.delacruz@solirius.com>
2026-06-05 15:58:13 +0100

    added template routes for edit case
    - imported and mounted template sub routes for edit-case inside routes.js

--- app/routes.js
+++ app/routes.js
@@ -7,6 +7,16 @@ import createCaseRouter from './sub-routes/create-a-case.js';
 import caseDetailsRouter from './sub-routes/case-details.js';
 import caseAuditLogRouter from './sub-routes/case-audit-log.js';
 
+// import edit case sub-routers
+import editCaseTeamRouter from './sub-routes/edit-case/case-team.js';
+import editContactsRouter from './sub-routes/edit-case/contacts.js';
+import editDatesRouter from './sub-routes/edit-case/dates.js';
+import editDetailsRouter from './sub-routes/edit-case/details.js';
+import editEiaRouter from './sub-routes/edit-case/eia.js';
+import editFeeRouter from './sub-routes/edit-case/fee.js';
+import editOverviewRouter from './sub-routes/edit-case/overview.js';
+import editCaseRepsPeriodRouter from './sub-routes/edit-case/reps-period.js';
+
 // import helpers
 import { 
   getCase, 
@@ -32,6 +42,17 @@ const router = govukPrototypeKit.requests.setupRouter();
 router.use('/', createCaseRouter);
 router.use('/', caseDetailsRouter);
 router.use('/', caseAuditLogRouter);
+
+// mount edit case sub-routers
+router.use('/', editCaseTeamRouter);
+router.use('/', editContactsRouter);
+router.use('/', editDatesRouter);
+router.use('/', editDetailsRouter);
+router.use('/', editEiaRouter);
+router.use('/', editFeeRouter);
+router.use('/', editOverviewRouter);
+router.use('/', editCaseRepsPeriodRouter);
+
 
 // New routes below this
 
---
+++ app/sub-routes/edit-case/dates.js
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import { 
+    updateCaseData,
+    addAuditLog,
+    getCase
+} from '../../helpers.js';
+
+const router = Router();
+
+// routes below this
+
+
+export default router;
