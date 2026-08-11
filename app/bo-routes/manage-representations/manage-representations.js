3a8611c46ae16392209242e98133246130717edd
kierandelacruz-solirius <kieran.delacruz@solirius.com>
2026-08-11 13:57:14 +0100

    added pagination logic to manage reps

--- app/bo-routes/manage-representations/manage-representations.js
+++ app/bo-routes/manage-representations/manage-representations.js
@@ -14,18 +14,27 @@ router.get('/current-service/back-office/manage-representations/manage-represent
     return res.redirect('/current-service/back-office/cases'); 
   }
 
-  // handle flash messages (green success banners) for case edits
-  const sectionToJumpTo = req.session.data['flashSection'];
-  const flashMessage = req.session.data['flashMessage'];
-  
-  req.session.data['flashSection'] = null; 
-  req.session.data['flashMessage'] = null;
+    // showing results and pagination logic
+    //  find total number of reps
+    let totalReps = 0;
+    if (foundCase && foundCase.representations) {
+        totalReps = foundCase.representations.length;
+    }
 
+    // set up pagination logic
+    const itemsPerPage = 25; // 25 | 50 | 100 options
+    const currentPage = parseInt(req.query.page) || 1;
+
+    // calculate start and end numbers
+    const startItem = totalReps === 0 ? 0 : ((currentPage - 1) * itemsPerPage) + 1;
+    const endItem = Math.min(currentPage * itemsPerPage, totalReps);
+    
   // Render the page
   res.render('current-service/back-office/manage-representations/manage-representations', { 
-    currentCase: foundCase,       
-    flashSection: sectionToJumpTo,
-    flashMessage: flashMessage
+    currentCase: foundCase,
+    totalReps: totalReps,
+    startItem: startItem,
+    endItem: endItem
   });
 });
 
